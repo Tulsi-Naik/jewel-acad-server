a72ef3e9bba24c981877ae0277d75ed4b0035e02
Tulsi_Naik <tulsivnaik@gmail.com>
2025-06-25 13:23:03 +0530

    fix: apply CORS for Vercel frontend

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://jewellery-hub-two.vercel.app',
+  credentials: true
+}));
+
+
 app.use(express.json());
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
@@ -35,3 +43,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on ${PORT}`);
 });
+
