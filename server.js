560c5b1489705ce98f5b932dc7a3471bea7c98c9
Tulsi_Naik <tulsivnaik@gmail.com>
2025-06-30 08:42:06 +0530

    server modified

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (!MONGO_URI) {
   process.exit(1);
 }
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
+  useNewUrlParser: true,//
   useUnifiedTopology: true
 }).then(() => {
   console.log('MongoDB connected');
