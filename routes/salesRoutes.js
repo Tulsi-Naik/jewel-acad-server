53f996fd0149f915446e5f320de8c5944144ddb6
Tulsi_Naik <tulsivnaik@gmail.com>
2025-07-03 21:29:21 +0530

    get sales

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/salesController');
 
 router.post('/', controller.recordSale);
+router.get('/', (req, res) => {
+  res.json({ message: 'Sales route is working and protected' });
+});
 
 module.exports = router;
