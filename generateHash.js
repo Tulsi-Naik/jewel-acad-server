86d3650dafbea875cfcace3dda873274788b18e0
Tulsi_Naik <tulsivnaik@gmail.com>
2025-07-04 14:59:05 +0530

    env url

--- generateHash.js
+++ generateHash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
-const plainPassword = 'vendor123';
+const plainPassword = 'admin123';
 
 bcrypt.hash(plainPassword, 10).then(hash => {
   console.log('Hashed password:', hash);
