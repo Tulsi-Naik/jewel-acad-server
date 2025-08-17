b320697b709aa9162a78c36f2ae7e0f756795c11
Tulsi_Naik <tulsivnaik@gmail.com>
2025-08-17 12:53:30 +0530

    removed auth from reports

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,15 +1,11 @@
-
-
-//routes//reportRoutes.js
+// reportRoutes.js
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/reportController');
-const authenticateToken = require('../middleware/authMiddleware'); //  add this
 
-//  protect both endpoints so req.user is available for getDbForUser
-router.get('/daily', authenticateToken, controller.getDailyReport);
-router.get('/monthly', authenticateToken, controller.getMonthlyReport);
-router.get('/top-products', authenticateToken, controller.getTopProducts);
+// no need to add authenticateToken here
+router.get('/daily', controller.getDailyReport);
+router.get('/monthly', controller.getMonthlyReport);
+router.get('/top-products', controller.getTopProducts);
 
 module.exports = router;
-//
